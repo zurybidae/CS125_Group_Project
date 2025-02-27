a1952c227432e66bd468220b8f338b31368a5850
zurybidae <zurybidae@gmail.com>
2025-02-27 11:54:36 -0700

    Add files via upload

--- hangman_code_begining.c
+++ hangman_code_begining.c
@@ -5,59 +5,129 @@
 */
 
 
+//https://stackoverflow.com/questions/1478932/check-if-user-inputs-a-letter-or-number-in-c 
+//for checking is digit
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
-char word[15];
+char word[15]={"nope"};
 
 
 
 void word_picker(int x)
 {
-  strand(time(NULL));
+  srand(time(NULL));
   int ran_num = rand()%5+1;
 
-  if(x==1)
+  if(x==1)    //animal
   {
-    //animal
     switch(ran_num)
     {
     case 1:
     {
-      word={"giraffe"};
+      strcpy(word, "giraffe");
+      break;
     }
     case 2:
     {
-      word={"bobcat"};
+      strcpy(word, "bobcat");
+      break;
     }
     case 3:
     {
-      word={"orcha"};
+      strcpy(word, "orcha");
+      break;
     }
     case 4:
     {
-      word={"quail"};
+      strcpy(word, "quail");
+      break;
     }
     case 5:
     {
-      word={"muskrat"};
+      strcpy(word, "muskrat");
+      break;
     }
     default:
     {
       printf("oops");
+      break;
+    }
     }
   }
-
-  else if(x==2)
+  else if(x==2)  //places       
   {
-    //places
+    switch(ran_num)
+    {
+    case 1:
+    {
+      strcpy(word, "office");
+      break;
+    }
+    case 2:
+    {
+      strcpy(word, "paris");
+      break;
+    }
+    case 3:
+    {
+      strcpy(word, "whyoming");
+      break;
+    }
+    case 4:
+    {
+      strcpy(word, "disnyland");
+      break;
+    }
+    case 5:
+    {
+      strcpy(word, "playground");
+      break;
+    }
+    default:
+    {
+      printf("oops");
+      break;
+    }
+    }
   }
-  else
+  else  //food 
   {
-    //food 
+    switch(ran_num)
+    {
+    case 1:
+    {
+      strcpy(word, "cheese");
+      break;
+    }
+    case 2:
+    {
+      strcpy(word, "beagle");
+      break;
+    }
+    case 3:
+    {
+      strcpy(word, "eggs");
+      break;
+    }
+    case 4:
+    {
+      strcpy(word, "apple");
+      break;
+    }
+    case 5:
+    {
+      strcpy(word, "nachos");
+      break;
+    }
+    default:
+    {
+      printf("oops");
+      break;
+    }
   }
-
 }
 
 
@@ -68,10 +138,10 @@ int main()
   printf("Welcome to Hangman!\n\n");
   printf("Chose a catagory of words:\n");
   printf("1) Animals  2) Places 3) Foods \n");
-  scanf("%d", choice);                          //needs varificvation system
+  scanf("%d", &choice);                          //needs varificvation system
 
-  
-  if(
+  word_picker(choice);
+  printf("%s%s", word[0], word[1]);
 
   return 0;
 }
