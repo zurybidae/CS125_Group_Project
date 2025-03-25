9db3e211a835d5aaeaad3c13ebe356520fdc5da7
natalex1212 <ALEXANDN@my.erau.edu>
2025-03-25 11:43:42 -0700

    Create guy_printer.h

---
+++ guy_printer.h
@@ -0,0 +1,70 @@
+void guy_printer(int x) //-----------------------------------------------------------------------------------------------------------------------------------------
+{
+    
+    if(x==0)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |      \n");
+        printf("   |      \n");
+        printf("   |      \n");
+        printf("___|_____ \n");
+    }
+    else if(x==1)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |      \n");
+        printf("   |      \n");
+        printf("___|_____ \n");
+    }
+    else if(x==2)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |   |  \n");
+        printf("   |      \n");
+        printf("___|_____ \n");
+    }
+    else if(x==3)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |  /|  \n");
+        printf("   |      \n");
+        printf("___|_____ \n");
+    }
+    else if(x==4)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |  /|\\ \n");
+        printf("   |      \n");
+        printf("___|_____ \n");
+    }
+    else if(x==5)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |  /|\\ \n");
+        printf("   |  /   \n");
+        printf("___|_____ \n");
+    }
+    else if(x==6)
+    {
+        printf("    _ _   \n");
+        printf("   |   |  \n");    
+        printf("   |   0  \n");
+        printf("   |  /|\\ \n");
+        printf("   |  / \\ \n");
+        printf("___|_____ \n");
+        
+       
+    }
+    
+}
