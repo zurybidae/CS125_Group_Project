3ef41cb6a286df647b44b3d5c479ac6a8a817eeb
natalex1212 <ALEXANDN@my.erau.edu>
2025-03-30 19:29:51 -0700

    Update aaa_print_the_stuff.c

--- aaa_print_the_stuff.c
+++ aaa_print_the_stuff.c
@@ -1,92 +1,98 @@
 #include <stdio.h>
 
-void print(char word2[], int length, char filler[])  //-----------------------------------------------------
-{
-    int i;    
 
-    for(i=0;i<length;i++)
-    {
-        if (word2[i]!='x')
-        {
-           printf("%c ", word2[i]);
-           
-        }
-        else
-        {
-            printf("%c ", filler[i]);
-        }
-    }
-}
+//This code allows the player to guess the correct word. If they guess the entire word correctly then the word will be printed,
+// but if they guess it wrong the word will be printed with underscores instead of letters.
+
+
+void print(char word2[], int length, char filler[])  //-----------------------------------------------------
+  {
+     int i;    
+
+     for(i=0;i<length;i++)
+       {
+          if (word2[i]!='x')
+            {
+               printf("%c ", word2[i]);
+            }
+          else
+            {
+               printf("%c ", filler[i]);
+            }
+       }
+  } 
+
+
+//This code is the animation for the hangman game. For every incorrect guess the player makes, the hangman will get another limb. 
+//After six incorrect guesses the hangman will be complete and the player will lose.
 
 
 void guy_printer(int x) 
-{
+  { 
+     if(x==0)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |      \n");
+          printf("   |      \n");
+          printf("   |      \n");
+          printf("___|_____ \n");
+       }
+     else if(x==1)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |      \n");
+          printf("   |      \n");
+          printf("___|_____ \n");
+       }
+     else if(x==2)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |   |  \n");
+          printf("   |      \n");
+          printf("___|_____ \n");
+       }
+     else if(x==3)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |  /|  \n");
+          printf("   |      \n");
+          printf("___|_____ \n");
+       }
+     else if(x==4)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |  /|\\ \n");
+          printf("   |      \n");
+          printf("___|_____ \n");
+       }
+     else if(x==5)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |  /|\\ \n");
+          printf("   |  /   \n");
+          printf("___|_____ \n");
+       }
+     else if(x==6)
+       {
+          printf("    _ _   \n");
+          printf("   |   |  \n");    
+          printf("   |   0  \n");
+          printf("   |  /|\\ \n");
+          printf("   |  / \\ \n");
+          printf("___|_____ \n"); 
+       }
     
-    if(x==0)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |      \n");
-        printf("   |      \n");
-        printf("   |      \n");
-        printf("___|_____ \n");
-    }
-    else if(x==1)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |      \n");
-        printf("   |      \n");
-        printf("___|_____ \n");
-    }
-    else if(x==2)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |   |  \n");
-        printf("   |      \n");
-        printf("___|_____ \n");
-    }
-    else if(x==3)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |  /|  \n");
-        printf("   |      \n");
-        printf("___|_____ \n");
-    }
-    else if(x==4)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |  /|\\ \n");
-        printf("   |      \n");
-        printf("___|_____ \n");
-    }
-    else if(x==5)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |  /|\\ \n");
-        printf("   |  /   \n");
-        printf("___|_____ \n");
-    }
-    else if(x==6)
-    {
-        printf("    _ _   \n");
-        printf("   |   |  \n");    
-        printf("   |   0  \n");
-        printf("   |  /|\\ \n");
-        printf("   |  / \\ \n");
-        printf("___|_____ \n");
-        
-       
-    }
-    
-}
+  }
+
 
